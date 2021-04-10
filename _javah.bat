2755da239544f77c28246e28afa5320039d39a2e
gituser <gituser@localhost>
2021-04-10 20:57:55 +0900

    プロジェクトを変換 gitignore を追加

--- _javah.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Java\jdk1.6.0_29\bin\javah.exe" -classpath bin/classes -d jni jp.dip.muracoro.comitton.stream.CallJniLibrary
-"C:\Program Files\Java\jdk1.6.0_29\bin\javah.exe" -classpath bin/classes -d jni jp.dip.muracoro.comitton.stream.CallImgLibrary
